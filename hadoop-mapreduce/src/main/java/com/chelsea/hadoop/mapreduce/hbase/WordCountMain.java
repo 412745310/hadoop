a8096d135485235bf7ebcb22883a43a7921574cc
baojun_a <baojun_a@aspirecn.com>
2019-05-07 16:51:53 +0800

    修改initTableReducerJob参数

--- hadoop-mapreduce/src/main/java/com/chelsea/hadoop/mapreduce/hbase/WordCountMain.java
+++ hadoop-mapreduce/src/main/java/com/chelsea/hadoop/mapreduce/hbase/WordCountMain.java
@@ -17,8 +17,8 @@ public class WordCountMain {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://47.107.247.223:9000");
-        conf.set("hbase.zookeeper.quorum", "47.107.247.223");
+        conf.set("fs.defaultFS", "hdfs://172.18.20.237:9000");
+        conf.set("hbase.zookeeper.quorum", "172.18.20.237");
         Job job = Job.getInstance(conf);
         // 指定jar包运行主类
         job.setJarByClass(WordCountMain.class);
@@ -28,7 +28,8 @@ public class WordCountMain {
         job.setMapOutputKeyClass(Text.class);
         // 指定mapper类输出value类型
         job.setMapOutputValueClass(IntWritable.class);
-        TableMapReduceUtil.initTableReducerJob("wordCount", WordCountReducer.class, job, null, null, null, null, false);
+        // 本地测试最后一个参数设置为false
+        TableMapReduceUtil.initTableReducerJob("wordCount", WordCountReducer.class, job, null, null, null, null, true);
         // 指定mr数据的输入路径
         FileInputFormat.setInputPaths(job, "/wordcount/input");
         // 提交程序，并且监控打印程序执行情况
